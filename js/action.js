31716b399646dba9e25a493a426bc2b266d5d7af
Leandro Chazarreta <91677149+Chazmann@users.noreply.github.com>
2024-04-03 01:01:12 -0300

    primer repo. dom js

---
+++ js/action.js
@@ -0,0 +1,44 @@
+let counter = 1;
+let header = document.getElementById("head");
+let element = document.getElementById("nro1");
+let texto = document.getElementById("btn");
+element.innerHTML = `Nro de clics ${counter}`;
+
+function e() {
+  counter = counter + 1;
+  element.innerHTML = `Nro de clics ${counter}`;
+  header.innerHTML = `<b>Jodiendo con el Js DOM</b>`;
+  //cambio el texto del boton
+  if (texto.innerHTML === "Seguir") {
+    texto.classList.add("active")
+    texto.innerHTML = "Siguiendo";
+  } else {
+    texto.innerHTML = "Seguir";
+    texto.classList.remove("active")
+    texto.classList.remove("hover")
+  }
+  element.classList.toggle("btn");
+  console.log(texto.textContent, counter);
+}
+
+function remove() {
+  let txtbtn = document.getElementById("btn");
+
+  if (txtbtn.innerHTML === "Siguiendo") {
+       texto.innerHTML = "Dejar de seguir";
+    txtbtn.classList.add("hover");
+
+  } else if (txtbtn.innerHTML === "Dejar de seguir") {
+    txtbtn.classList.remove("hover");
+    texto.innerHTML = "Siguiendo";
+  }
+}
+
+function siguiendo() {
+  let txttn = document.getElementById("btn");
+  if (txttn.innerHTML === "Dejar de seguir") {
+    texto.innerHTML = "Siguiendo";
+    txttn.classList.remove("hover");
+    
+  }
+}
