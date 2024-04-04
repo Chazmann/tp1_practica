3f1c143c4bde9a65875352c2b923be7cdb9fef1c
Leandro Chazarreta <91677149+Chazmann@users.noreply.github.com>
2024-04-04 14:14:34 -0300

    AUTOCOMPLETE JS Y JQUERY

--- js/autocomp.js
+++ js/autocomp.js
@@ -1,29 +1,68 @@
 let listaAut = [
-    "Oncología Clínica",
-    "Cardiología",
-    "Psiquiatría",
-    "Medicina Legal",
-    "Nefrología",
-    "Clínica Pediátrica",
-    "Anemia",
-    "Medicina del Deporte",
-    "Medicina del Trabajo",
-    "Cirugía Plástica y Reparadora",
-    "Ortopedia y Traumatología",
-    "Ortodoncia y Ortopedia Maxilar",
-    "Implantología Oral",
-    "Odontopediatría",
-    "Terapia Intensiva",
-    "Cirugía y Traumatología Bucomáxilofacial",
-    "Microbiología Clínica",
-    "Infectología",
-    "Oftalmología Infantil",
-    "Kinesiología Cardio-Respiratoria",
-  ];
+  "Oncología Clínica",
+  "Cardiología",
+  "Psiquiatría",
+  "Medicina Legal",
+  "Nefrología",
+  "Clínica Pediátrica",
+  "Anemia",
+  "Medicina del Deporte",
+  "Medicina del Trabajo",
+  "Cirugía Plástica y Reparadora",
+  "Ortopedia y Traumatología",
+  "Ortodoncia y Ortopedia Maxilar",
+  "Implantología Oral",
+  "Odontopediatría",
+  "Terapia Intensiva",
+  "Cirugía y Traumatología Bucomáxilofacial",
+  "Microbiología Clínica",
+  "Infectología",
+  "Oftalmología Infantil",
+  "Kinesiología Cardio-Respiratoria",
+];
+//INYECCION DE DATOS MEDIANTE LECTURA DE ARRAY
+ listaAut.forEach((elem) => {
+   let dataList = document.querySelector("#especialidades");
+   option = document.createElement("option");
+   option.value = elem;
+  dataList.append(option);
+ });
 
-//ordeno la lista
-let dataList = document.querySelector("#espe")
-let txtauto = document.querySelector("#busca")
 
+ //INYECCION DE DATOS MEDIANTE JQUERY
+$( function() {
+    $( "#especi" ).autocomplete({
+    source: listaAut
+  });
+} );
 
-console.log(txtauto)
+// MODIFICACION DE MENSAJES TRAS EL RESULTADO DE BUSQUEDA
+
+$.widget( "ui.autocomplete", $.ui.autocomplete, {
+	options: {
+		messages: {
+			noResults: "No se encontraron resultados.",
+			results: function( amount ) {
+				return amount + ( amount > 1 ? " resultados encontrados" : " resultado encontrado" ) +
+					", usar las flechas hacia arriba y abajo para navegar.";
+			}
+		}
+	},
+
+	__response: function( content ) {
+		var message;
+		this._superApply( arguments );
+		if ( this.options.disabled || this.cancelSearch ) {
+			return;
+		}
+		if ( content && content.length ) {
+			message = this.options.messages.results( content.length );
+		} else {
+			message = this.options.messages.noResults;
+		}
+		clearTimeout( this.liveRegionTimer );
+		this.liveRegionTimer = this._delay( function() {
+			this.liveRegion.html( $( "" ).text( message ) );
+		}, 1000 );
+	}
+} );
